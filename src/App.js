791aa5cce72096f613429f335057832fd1bc5b45
Joseph Edmonds <consultingjde@gmail.com>
2021-08-18 15:51:37 -0700

    ConfigureStore and Provider in Index

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-
+      <h1>The Count is: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase the count by 1</button>
+      <button onClick={() => setCount(count - 1)}>decrease the count by 1</button>
     </div>
   );
 }
 
 export default App;
+
