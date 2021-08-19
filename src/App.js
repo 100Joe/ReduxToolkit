a1a674542a901eb3dbc9adb3588c9469f6f243bf
Joseph Edmonds <consultingjde@gmail.com>
2021-08-18 17:20:06 -0700

    updating reducers and wrapping up toolkit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 
-import React, { useState } from 'react';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
+import { increment, decrement, incrementByAmount, decrementByAmount } from './redux/counter';
 
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const count = useSelector(state => state.counter.count);
+  const { count } = useSelector(state => state.counter);
+
+  // use the dispatch hook to call the action which triggers the state to be manipulated. 
+  // This hook be used to pull any actions out of any of the reducers in the app. 
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <h1>The Count is: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increase the count by 1</button>
-      <button onClick={() => setCount(count - 1)}>decrease the count by 1</button>
+      <button onClick={() => dispatch(increment())}>Increase the count by 1</button>
+      <button onClick={() => dispatch(decrement())}>decrease the count by 1</button>
+      <button onClick={() => dispatch(incrementByAmount(14))}>increase the count by 14</button>
+      <button onClick={() => dispatch(decrementByAmount(20))}>decrease the count by 20</button>
     </div>
   );
 }
